ce80dbacb0e48c6520c13fc858f0d8ba9060961b
doanhv <doan.hoa@tripath.vn>
2020-10-03 01:13:20 +0700

    deployment

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,9 @@ module.exports = {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
     types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
   },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/'
+        : '/'
 }
 
 function addStyleResource (rule) {
